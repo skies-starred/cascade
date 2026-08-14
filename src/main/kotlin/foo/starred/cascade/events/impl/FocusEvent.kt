86b39a8d76d5305514c4087f67766d378847f951
skies-starred <github@starred.foo>
2026-08-14 09:39:12 +0100

    feat: focus events

---
+++ src/main/kotlin/foo/starred/cascade/events/impl/FocusEvent.kt
@@ -0,0 +1,14 @@
+﻿package foo.starred.cascade.events.impl
+
+import foo.starred.cascade.events.base.UIEvent
+import foo.starred.cascade.primitives.base.impl.IPrimitiveElement
+
+sealed class FocusEvent {
+    data class Gain(
+        val element: IPrimitiveElement<*>
+    ) : UIEvent()
+
+    data class Lose(
+        val element: IPrimitiveElement<*>
+    ) : UIEvent()
+}
