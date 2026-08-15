7f03daf9eaab81d1b4baa3d6569c1a72b2138e4d
skies-starred <github@starred.foo>
2026-08-15 11:08:03 +0100

    feat(CenterPositionConstraint): offsets

--- src/main/kotlin/foo/starred/cascade/constraints/impl/position/CenterPositionConstraint.kt
+++ src/main/kotlin/foo/starred/cascade/constraints/impl/position/CenterPositionConstraint.kt
@@ -3,12 +3,15 @@ package foo.starred.cascade.constraints.impl.position
 import foo.starred.cascade.constraints.base.IPositionConstraint
 import foo.starred.cascade.primitives.base.impl.IPrimitiveElement
 
-class CenterPositionConstraint : IPositionConstraint {
+class CenterPositionConstraint(x: Number = 0f, y: Number = 0f) : IPositionConstraint {
+    val x = x.toFloat()
+    val y = y.toFloat()
+
     override fun x(element: IPrimitiveElement<*>, parent: IPrimitiveElement<*>): Float {
-        return parent.x + (parent.width - element.width) / 2f
+        return (parent.x + (parent.width - element.width) / 2f) + x
     }
 
     override fun y(element: IPrimitiveElement<*>, parent: IPrimitiveElement<*>): Float {
-        return parent.y + (parent.height - element.height) / 2f
+        return (parent.y + (parent.height - element.height) / 2f) + y
     }
 }
