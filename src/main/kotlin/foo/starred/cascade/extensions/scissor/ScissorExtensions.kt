0d2ea0feeb28e648e1190e52aa040ff3f99dc112
skies-starred <github@starred.foo>
2026-08-19 17:38:44 +0100

    internal(scissor): hmmmmmmm :(

--- src/main/kotlin/foo/starred/cascade/extensions/scissor/ScissorExtensions.kt
+++ src/main/kotlin/foo/starred/cascade/extensions/scissor/ScissorExtensions.kt
@@ -1,7 +1,7 @@
 package foo.starred.cascade.extensions.scissor
 
-import foo.starred.cascade.Cascade.client
 import net.minecraft.client.gui.GuiGraphicsExtractor
+import net.minecraft.client.gui.navigation.ScreenRectangle
 
 inline fun GuiGraphicsExtractor.scissor(x: Number, y: Number, width: Number, height: Number, block: () -> Unit) {
     val x = x.toInt()
@@ -14,11 +14,20 @@ inline fun GuiGraphicsExtractor.scissor(x: Number, y: Number, width: Number, hei
 
     val x00 = maxOf(0, x)
     val y00 = maxOf(0, y)
-    val x01 = minOf(client.window.guiScaledWidth, x + width)
-    val y01 = minOf(client.window.guiScaledHeight, y + height)
+    val x01 = minOf(guiWidth(), x + width)
+    val y01 = minOf(guiHeight(), y + height)
 
     if (x01 <= x00) return
     if (y01 <= y00) return
+
+    val screen = ScreenRectangle(x00, y00, x01 - x00, y01 - y00).transformAxisAligned(pose())
+    val scissor = scissorStack.peek()
+
+    if (scissor != null) {
+        val intersection = scissor.intersection(screen) ?: return
+        if (intersection.width() <= 0) return
+        if (intersection.height() <= 0) return
+    }
 
     enableScissor(x00, y00, x01, y01)
     block()
