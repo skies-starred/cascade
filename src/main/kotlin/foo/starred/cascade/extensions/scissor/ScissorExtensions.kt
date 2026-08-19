31dcbc5c380c92e496f5404b065952721116a74e
skies-starred <github@starred.foo>
2026-08-19 13:05:06 +0100

    internal(ScissorExtensions): oops

--- src/main/kotlin/foo/starred/cascade/extensions/scissor/ScissorExtensions.kt
+++ src/main/kotlin/foo/starred/cascade/extensions/scissor/ScissorExtensions.kt
@@ -1,5 +1,6 @@
 package foo.starred.cascade.extensions.scissor
 
+import foo.starred.cascade.Cascade.client
 import net.minecraft.client.gui.GuiGraphicsExtractor
 
 inline fun GuiGraphicsExtractor.scissor(x: Number, y: Number, width: Number, height: Number, block: () -> Unit) {
@@ -11,7 +12,15 @@ inline fun GuiGraphicsExtractor.scissor(x: Number, y: Number, width: Number, hei
     if (width <= 0) return
     if (height <= 0) return
 
-    enableScissor(x, y, x + width, y + height)
+    val x00 = maxOf(0, x)
+    val y00 = maxOf(0, y)
+    val x01 = minOf(client.window.guiScaledWidth, x + width)
+    val y01 = minOf(client.window.guiScaledHeight, y + height)
+
+    if (x01 <= x00) return
+    if (y01 <= y00) return
+
+    enableScissor(x00, y00, x01, y01)
     block()
     disableScissor()
 }
