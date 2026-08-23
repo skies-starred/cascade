74bb19ef8e6eaad25be08bba36d1ddd67c2cbe8b
skies-starred <github@starred.foo>
2026-08-23 15:41:19 +0100

    fix(IPrimitiveElement): ConcurrentModificationException

--- src/main/kotlin/foo/starred/cascade/primitives/base/interfaces/IPrimitiveEffects.kt
+++ src/main/kotlin/foo/starred/cascade/primitives/base/interfaces/IPrimitiveEffects.kt
@@ -2,9 +2,10 @@ package foo.starred.cascade.primitives.base.interfaces
 
 import foo.starred.cascade.effects.base.IEffect
 import foo.starred.cascade.primitives.base.impl.IPrimitiveElement
+import java.util.concurrent.CopyOnWriteArrayList
 
-interface IPrimitiveEffect<T> : IPrimitiveSelf<T> where T : IPrimitiveElement<T> {
-    val effects: MutableList<IEffect>
+interface IPrimitiveEffects<T> : IPrimitiveSelf<T> where T : IPrimitiveElement<T> {
+    val effects: CopyOnWriteArrayList<IEffect>
 
     fun <E : IEffect> effect(effect: E): E {
         effect.element = self
