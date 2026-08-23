3ea30c445934930fc7a490d459b6c2a187e0595d
skies-starred <github@starred.foo>
2026-08-23 14:49:54 +0100

    internal: optimise imports

--- src/main/kotlin/foo/starred/cascade/primitives/impl/ArcPrimitive.kt
+++ src/main/kotlin/foo/starred/cascade/primitives/impl/ArcPrimitive.kt
@@ -1,7 +1,7 @@
 package foo.starred.cascade.primitives.impl
 
-import foo.starred.cascade.primitives.base.impl.IPrimitiveElement
 import foo.starred.cascade.graphics.extensions.arc.arc
+import foo.starred.cascade.primitives.base.impl.IPrimitiveElement
 import net.minecraft.client.gui.GuiGraphicsExtractor
 import kotlin.math.max
 
