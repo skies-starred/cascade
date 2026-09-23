696eb72db0e6f0e7f454bdb8ad7ea1d013c00759
skies-starred <github@starred.foo>
2026-09-23 09:56:00 +0100

    bump(version): 2026.09.4

--- src/main/kotlin/foo/starred/cascade/Cascade.kt
+++ src/main/kotlin/foo/starred/cascade/Cascade.kt
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 object Cascade : ClientModInitializer {
-    const val version: String = /*$ mod_version*/ "2026.09.3"
+    const val version: String = /*$ mod_version*/ "2026.09.4"
     const val id: String = /*$ mod_id*/ "cascade"
     const val name: String = /*$ mod_name*/ "Cascade"
 
